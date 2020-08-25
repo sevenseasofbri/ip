8e4d8db3e72a8402be75222a2e6c689fb5979784
sevenseasofbri <vishruti250102@gmail.com>
2020-08-25 23:56:51 +0800

    Add Increment: Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class Duke {
-    public static void printList(String[] items){
+    public static void printList(Task[] items, int numItems){
         System.out.println("\t_______________________~Your List~__________________________");
-        for(int i=0; i<items.length; i++){
-            System.out.println("\t"+(i+1)+". "+items[i]);
+        for(int i=0; i<numItems; i++){
+            System.out.println("\t"+(i+1)+". ["+items[i].getStatusIcon()+"] "+items[i].description);
         }
         System.out.println("\t____________________________________________________________");
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
-        int numItems =0;
+        Task[] list = new Task[100];
+        int numItems =0, totalTasksDone=0;
         String answer="";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -27,10 +27,29 @@ public class Duke {
                 break;
             }
             if(answer.trim().equalsIgnoreCase("list")){
-                printList(Arrays.copyOf(list, numItems));
+                printList(list, numItems);
+                continue;
+            }
+            if(answer.indexOf("done") > -1){
+                answer = answer.trim();
+                String[] words = answer.split(" ");
+                if(words.length>1) {
+                    int valueMarkedDone = Integer.parseInt(words[1]);
+                    if(valueMarkedDone>0 && valueMarkedDone<=numItems) {
+                        list[valueMarkedDone - 1].markAsDone();
+                        totalTasksDone++;
+                        System.out.println("\tAwesome! I've marked this task as done:");
+                        System.out.println("\t["+list[valueMarkedDone - 1].getStatusIcon()+"] "+list[valueMarkedDone - 1].description);
+                        System.out.println("\tOnly "+(numItems-totalTasksDone)+" to go! ;)");
+                    }
+                    else{
+                        System.out.println("\tSorry could not mark as done! Please enter a valid list number.");
+                    }
+                }
             }
             else {
-                list[numItems] = answer;
+                Task newTask = new Task(answer);
+                list[numItems] = newTask;
                 numItems++;
                 System.out.println("\tAdded:" + answer);
             }
