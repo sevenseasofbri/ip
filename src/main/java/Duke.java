9085a679874f61c10a452d2f2849cf60a2263b93
sevenseasofbri <vishruti250102@gmail.com>
2020-08-25 22:55:43 +0800

    Add Increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,16 @@
 import java.util.*;
 public class Duke {
+    public static void printList(String[] items){
+        System.out.println("\t_______________________~Your List~__________________________");
+        for(int i=0; i<items.length; i++){
+            System.out.println("\t"+(i+1)+". "+items[i]);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int numItems =0;
         String answer="";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,8 +26,16 @@ public class Duke {
             if(answer.trim().equalsIgnoreCase("bye")){
                 break;
             }
-            System.out.println("\t"+answer);
+            if(answer.trim().equalsIgnoreCase("list")){
+                printList(Arrays.copyOf(list, numItems));
+            }
+            else {
+                list[numItems] = answer;
+                numItems++;
+                System.out.println("\tAdded:" + answer);
+            }
         }
+        System.out.println("\t____________________________________________________________");
         System.out.println("\tFarewell. Until next time my dude.");
         System.out.println("\t____________________________________________________________");
     }
