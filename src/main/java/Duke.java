5d6a7851808ff7609635b73d6bcf2be886466959
sevenseasofbri <vishruti250102@gmail.com>
2020-08-25 22:37:25 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,25 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String answer="";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm\n"+ logo);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm\n"+ logo);
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        while(true){
+            answer = in.nextLine();
+            if(answer.trim().equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println("\t"+answer);
+        }
+        System.out.println("\tFarewell. Until next time my dude.");
+        System.out.println("\t____________________________________________________________");
     }
 }
